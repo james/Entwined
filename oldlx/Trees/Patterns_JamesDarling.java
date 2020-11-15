13587fbae9f775551855360d894bcfcb910934d9
James Darling <james@abscond.org>
2020-11-15 22:58:22 +0000

    Make rainbow coloured trees!
    So I've now figured out *a way* of setting colours on cubes. For now, I am selecting the colour of the cube based on it's `treeIndex`, so each tree/shrub is a different colour from the choices from the LGBT Pride flag.
    
    They are rotating based on the SawLFO, but this is obviously not the correct way to do this, that's my next job.

--- oldlx/Trees/Patterns_JamesDarling.java
+++ oldlx/Trees/Patterns_JamesDarling.java
@@ -12,6 +12,7 @@ class JamesTest extends TSPattern {
   private float maxx = -Float.MAX_VALUE;
   private float waveWidth = 1;
   private float speedMult = 1000;
+  private float colorIndex = 0;
 
   final BasicParameter speedParam = new BasicParameter("Speed", 5, 20, .01);
   final BasicParameter waveSlope = new BasicParameter("waveSlope", 360, 1, 720);
@@ -37,11 +38,49 @@ class JamesTest extends TSPattern {
 
     // Use a for loop here to set the cube colors
     for (Cube cube : model.cubes) {
-      colors[cube.index] = lx.hsb( (float)( (wave.getValuef() + waveSlope.getValuef() * Utils.map(cube.x, minx, maxx) ) % 360), 100, 100);
+      colorIndex = ((wave.getValuef() * 100) + cube.config.treeIndex) % 6;
+      if (colorIndex == 0) {
+        // Electric Red
+        colors[cube.index] = lx.hsb( 0, 100, 91 );
+      } else if (colorIndex == 1) {
+        // Dark Orange
+        colors[cube.index] = lx.hsb( 33, 100, 100 );
+      } else if (colorIndex == 2) {
+        // Canary Yellow
+        colors[cube.index] = lx.hsb( 56, 100, 100 );
+      } else if (colorIndex == 3) {
+        // La Salle Green
+        colors[cube.index] = lx.hsb( 134, 100, 51 );
+      } else if (colorIndex == 4) {
+        // Blue
+        colors[cube.index] = lx.hsb( 224, 100, 100 );
+      } else if (colorIndex == 5) {
+        // Patriarch
+        colors[cube.index] = lx.hsb( 292, 100, 54 );
+      }
     }
 
     for (ShrubCube cube : model.shrubCubes) {
-      colors[cube.index] = lx.hsb( (float)( (wave.getValuef() + waveSlope.getValuef() * Utils.map(cube.x, minx, maxx) ) % 360), 100, 100);
+      colorIndex = ((wave.getValuef() * 100) + cube.config.shrubIndex) % 6;
+      if (colorIndex == 0) {
+        // Electric Red
+        colors[cube.index] = lx.hsb( 0, 100, 91 );
+      } else if (colorIndex == 1) {
+        // Dark Orange
+        colors[cube.index] = lx.hsb( 33, 100, 100 );
+      } else if (colorIndex == 2) {
+        // Canary Yellow
+        colors[cube.index] = lx.hsb( 56, 100, 100 );
+      } else if (colorIndex == 3) {
+        // La Salle Green
+        colors[cube.index] = lx.hsb( 134, 100, 51 );
+      } else if (colorIndex == 4) {
+        // Blue
+        colors[cube.index] = lx.hsb( 224, 100, 100 );
+      } else if (colorIndex == 5) {
+        // Patriarch
+        colors[cube.index] = lx.hsb( 292, 100, 54 );
+      }
     }
   }
 }
